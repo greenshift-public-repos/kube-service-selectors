2cb69eee124d888e928ba14de7b83ae944700eea
Madhav Girish Nair <madhavgirish02@gmail.com>
2026-04-17 14:36:46 +0200

    Update build.sh for supporting ACR pushing of image

--- build.sh
+++ build.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
 set -e
 
-OUTPUT=kube-service-selectors:build
-docker build -t ${OUTPUT} --label commit=$(git rev-parse HEAD) --label version=${VERSION:-'none'} .
+IMAGE_NAME=kube-service-selectors
+REGISTRY=""
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -r) REGISTRY="$2"; shift 2 ;;
+    *) shift ;;
+  esac
+done
+
+GIT_HASH=$(git rev-parse HEAD)
+
+docker build -t "${IMAGE_NAME}:build" \
+  --label commit="${GIT_HASH}" \
+  --label version="${VERSION:-none}" .
+
+if [[ -n "${REGISTRY}" ]]; then
+  REMOTE_TAG="${REGISTRY}/${IMAGE_NAME}:${GIT_HASH}"
+  docker tag "${IMAGE_NAME}:build" "${REMOTE_TAG}"
+  docker push "${REMOTE_TAG}"
+fi
